94769547c7658df485d2e6578d928afd5e2c2ac2
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-25 23:43:28 +0330

    Create template for post_list and post_detail.

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "post"
+app_name = "blog"
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="list"),
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("<slug:slug>", views.PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ class PostListView(generic.ListView):
     queryset = Post.published.order_by("-created_on")
     template_name = "blog/index.html"
     context_object_name = "posts"
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
